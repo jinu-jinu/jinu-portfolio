22feff8eddabf576521b9755b296010d4e97fe00
jinu <wlsdn5153@naver.com>
2024-04-24 15:24:50 +0900

    flow field 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,25 @@
-import { Suspense, useEffect } from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
-import SmoothScroll from "@/utils/SmoothScroll";
-import UserInterface from "@/components/UserInterface/UserInterface";
-import Home from "@/pages/Home/Home";
-import { Bridge, Loading } from "./components/Modal";
-import About from "./pages/About/About";
-import Error from "./pages/Error/Error";
-import Contact from "./pages/Contact/Contact";
+import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./components/3d/Experience";
-import { EffectComposer } from "@react-three/postprocessing";
-import Fluid from "./components/3d/Fluid/Fluid";
-import { AnimatePresence } from "framer-motion";
-import { isDarkMode } from "./utils/Utils";
-import { useLoadingActions } from "./stores/LoadingStore";
-import { useProgress } from "@react-three/drei";
-
-const AssetDownLoader = () => {
-  const handleAssetDownload = useLoadingActions("handleAssetDownload");
-  const navigate = useNavigate();
-  const { loaded, total } = useProgress();
-
-  useEffect(() => {
-    if (loaded === total) {
-      navigate("/");
-      handleAssetDownload(true);
-    }
-  }, [loaded, total]);
-  return null;
-};
 
 const App = () => {
-  useEffect(() => {
-    window.onbeforeunload = function () {
-      window.scrollTo(0, 0);
-    };
-
-    document.documentElement.classList.add(isDarkMode());
-  }, []);
-
-  console.log("app");
   return (
-    <>
-      <AssetDownLoader />
-      <SmoothScroll>
-        <>
-          <Canvas
-            camera={{ position: [0, 0, 1.5] }}
-            style={{
-              position: "fixed",
-              top: 0,
-              left: 0,
-              width: "100%",
-              height: "100vh",
-              backgroundColor: "transparent",
-              pointerEvents: "none",
-            }}
-          >
-            <Suspense fallback={null}>
-              <Experience />
-              <EffectComposer>
-                <Fluid />
-              </EffectComposer>
-            </Suspense>
-          </Canvas>
-
-          <div className="font-Prompt bg-white dark:bg-black text-black dark:text-white theme-transition">
-            <UserInterface />
-
-            <AnimatePresence mode="wait">
-              <Routes location={location} key={location.pathname}>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/*" element={<Error />} />
-              </Routes>
-            </AnimatePresence>
-
-            <Loading />
-            <Bridge />
-          </div>
-        </>
-      </SmoothScroll>
-    </>
+    <Canvas
+      camera={{ position: [0, 0, 1.5] }}
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100vh",
+        backgroundColor: "transparent",
+        pointerEvents: "none",
+      }}
+    >
+      <Suspense fallback={null}>
+        <Experience />
+      </Suspense>
+    </Canvas>
   );
 };
 
